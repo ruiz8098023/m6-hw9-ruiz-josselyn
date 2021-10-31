c87d102dc551a97461507226b1895eb69e84c16b
Josselyn Ruiz <josselynruiz@ufl.edu>
2021-10-31 08:32:20 -0400

    Updated JS; added preventDefault()

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
-var usersEl = document.getElementById('users')
-var btn = document.querySelector('button')
+var weatherEl = document.getElementById('weatherData')
+var btn = document.querySelector('form')
+var container = document.getElementById('container')
+var inputEl = document.querySelector('input')
 
-btn.onclick = function() {
+btn.onsubmit = function(e) {
     // FETCH, BOY!!!!!
+    e.preventDefault()
 
-    console.log('clicked')
-    fetch("api.openweathermap.org/data/2.5/weather?q=ocala,us&units=imperial&appid=2b0205684afdcd47c7543eaaa61080b6")
-    .then(function(res) {
-        return res.json()
+    var query = inputEl.value
+
+    if (!query) return 
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?appid=2a73efeae86f8a890c1d1c110285c8ef&units=imperial&q=' + query)
+
+    .then(function(response) {
+        return response.json()
     })
 
-    .then(function(res) {
-        console.log(res.results)
+    .then(function(result) {
 
-        // FOR EACH LOOP FUNCTION
-        res.results.forEach(function(weather) {          
-
-            // CREATE DIV
-            var container = document.createElement('div')
-
-            // CITY AND COUNTRY CODE
-            var location = document.createElement('h1')
-            location.value = weather.name + weather.sys.country
-            container.appendChild(location)
-
-            // WEATHER IS...
-            var weatherIs = document.createElement('h3')
-            weatherIs.textContent = weather.main
-            container.appendChild(weather)
-
-            // WEATHER ICON
-            var img = document.createElement('img')
-            img.src = weather.weather.icon
-            container.appendChild(img)
-
-            // TEMPERATURE IS...
-            var temp = document.createElement('h3')
-            temp.textContent = weather.main.temp
-            container.appendChild(temp)
-
-            // TEMPERATURE FEELS LIKE
-            var tempFeels = document.createElement('h3')
-            tempFeels.textContent = weather.main.feels_like
-            container.appendChild(tempFeels)
-
-            // PUT CONTAINER INTO MAIN IN HTML
-            usersEl.appendChild(container)
-        })
-
-        // FOR OF LOOP
-        // for (const user of res.results) {
-        //     console.log(user.cell)
-        // }
+        console.log(result)
+        showWeather(result)
     })
 
+    .catch(function(err) {
+        console.log(err)
+    })
+
+}
+
+
+function showWeather(weather) {    
+        
+        // SET DIV BACK TO BLANK
+        container.innerHTML = " "
+
+        // CITY AND COUNTRY CODE
+        var location = document.createElement('h3')
+        location.textContent = weather.name + ", " + weather.sys.country
+        container.appendChild(location)
+
+        // WEATHER IS...
+        var weatherIs = document.createElement('h3')
+        weatherIs.textContent = weather.weather[0].description
+        container.appendChild(weatherIs)
+
+        // WEATHER ICON
+        var img = document.createElement('img')
+        img.src = weather.weather.icon
+        container.appendChild(img)
+
+        // TEMPERATURE IS...
+        var temp = document.createElement('h3')
+        temp.textContent = weather.main.temp  + "degrees Fahrenheit"
+        container.appendChild(temp)
+
+        // TEMPERATURE FEELS LIKE
+        var tempFeels = document.createElement('h3')
+        tempFeels.textContent = "Feels Like " + weather.main.feels_like + "degrees Fahrenheit"
+        container.appendChild(tempFeels)
 }
